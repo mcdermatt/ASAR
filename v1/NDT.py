ac88e9c6841e35f5d6e2a6c2a23642c7dcb4bc04
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2021-09-02 18:29:45 -0400

    more work on debugging NDT

--- v1/NDT.py
+++ v1/NDT.py
@@ -88,19 +88,20 @@ def NDT(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True, along_track_demo = Fa
 
 			E = np.linalg.pinv(sigma)
 
-			# score_i = np.exp( (-(q).T.dot(E).dot(q) ) /2 ) # was this
-			# score_i = 10*np.exp( (-(q).T.dot(E).dot(q) ) /2 ) # test
+			# score_i = -np.exp( (-(q).T.dot(E).dot(q) ) /2 ) # was this
 			score_i = (q).T.dot(E).dot(q) #test
-
 
 			score += score_i
 
-			
+			#DEBUG THIS - be careful of mixing up x and p
 			#get jacobian
-			# J = jacobian(x, P_corrected[:,index])  #was this- using func from utils that I wrote for ICP
-			J = np.array([[1, 0, (-x[0]*np.sin(x[2]) - x[1]*np.cos(x[2]))[0] ],
-						  [0, 1, (x[0]*np.cos(x[2]) - x[1]*np.sin(x[2]))[0]  ]]) #as descibed in Biber paper
+			#using func from utils that I wrote for ICP
+			J = jacobian(x, P_corrected[:,index])  
+			#as described in Biber paper
+			# J2 = np.array([[1, 0, (-x[0]*np.sin(x[2]) - x[1]*np.cos(x[2]))[0] ],
+						  # [0, 1, (x[0]*np.cos(x[2]) - x[1]*np.sin(x[2]))[0]  ]]) 
 			# print(J)
+			# print(J2)
 
 			#update gradient-----
 			g += (q).T.dot(E).dot(J).T*(-score_i)
@@ -154,9 +155,13 @@ def NDT(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True, along_track_demo = Fa
 		# print("g",g)
 		# print("H^-1", np.linalg.pinv(H))
 
+		#was this
 		dx = np.linalg.pinv(H).dot(-g)
-		# print("dx ",dx)
 		x += dx
+
+		# dx_dumb = np.linalg.pinv(H.dot(np.linalg.pinv(-g.T)))
+		# print("dx ",dx, "\ndx_dumb ", dx_dumb)
+		# x += dx_dumb.T
 		# print("x = ",x)
 
 		#draw all points progressing through transformation
@@ -166,7 +171,7 @@ def NDT(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True, along_track_demo = Fa
 		# P_corrected = rot.dot(P_corrected) + t #test
 
 		#plot progression
-		ax.plot(P_corrected[0,:], P_corrected[1,:], color = (1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),0.0025), ls = '', marker = '.', markersize = 20)
+		# ax.plot(P_corrected[0,:], P_corrected[1,:], color = (1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),0.0025), ls = '', marker = '.', markersize = 20)
 
 
 	#draw transformed point set
@@ -187,212 +192,212 @@ def NDT(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True, along_track_demo = Fa
 
 
 
-def NDT_old(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True):
+# def NDT_old(Q,P,fig,ax, fid = 10, num_cycles = 1, draw = True):
 
-	"""
-	keeping this in the file in case I break something...
+# 	"""
+# 	keeping this in the file in case I break something...
 
-	from Peter Biber, 2003
+# 	from Peter Biber, 2003
 	
-		fid -> breaks map down into fid x fid pieces """
+# 		fid -> breaks map down into fid x fid pieces """
 
-	# 1) Build the NDT of the first scan.
-	# 2) Initialize the estimate for the parameters (by zero or
-	# 		by using odometry data).
-	# 3) For each sample of the second scan: Map the
-	# 		reconstructed 2D point into the coordinate frame of
-	# 		the first scan according to the parameters.
-	# 4) Determine the corresponding normal distributions
-	# 		for each mapped point.
-	# 5) The score for the parameters is determined by
-	# 		evaluating the distribution for each mapped point
-	# 		and summing the result.
-	# 6) Calculate a new parameter estimate by trying to
-	# 		optimize the score. This is done by performing one
-	# 		step of Newton’s Algorithm.
-	# 7) Goto 3 until a convergence criterion is met.
+# 	# 1) Build the NDT of the first scan.
+# 	# 2) Initialize the estimate for the parameters (by zero or
+# 	# 		by using odometry data).
+# 	# 3) For each sample of the second scan: Map the
+# 	# 		reconstructed 2D point into the coordinate frame of
+# 	# 		the first scan according to the parameters.
+# 	# 4) Determine the corresponding normal distributions
+# 	# 		for each mapped point.
+# 	# 5) The score for the parameters is determined by
+# 	# 		evaluating the distribution for each mapped point
+# 	# 		and summing the result.
+# 	# 6) Calculate a new parameter estimate by trying to
+# 	# 		optimize the score. This is done by performing one
+# 	# 		step of Newton’s Algorithm.
+# 	# 7) Goto 3 until a convergence criterion is met.
 
-	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	# speed things up by decimating points
-	# print("before ", np.shape(P))
-	# P = P[::10]
-	# print("after ", np.shape(P))
+# 	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 	# speed things up by decimating points
+# 	# print("before ", np.shape(P))
+# 	# P = P[::10]
+# 	# print("after ", np.shape(P))
 
-	#get point positions in 2d space and draw 1st and 2nd scans
-	pp1 = draw_scan(Q,fig,ax, pt = 0) # set as 2 to hide it so we can see ellipses
-	pp2 = draw_scan(P,fig,ax, pt = 1) #pt number assigns color for plotting
+# 	#get point positions in 2d space and draw 1st and 2nd scans
+# 	pp1 = draw_scan(Q,fig,ax, pt = 0) # set as 2 to hide it so we can see ellipses
+# 	pp2 = draw_scan(P,fig,ax, pt = 1) #pt number assigns color for plotting
 
-	P_corrected = pp2
+# 	P_corrected = pp2
 
-	#	---------------------------------------------------------
-	# 1) Build the NDT of the first scan.
-	E1 = subdivide_scan(pp1,fig,ax, fidelity = fid, pt =0)
+# 	#	---------------------------------------------------------
+# 	# 1) Build the NDT of the first scan.
+# 	E1 = subdivide_scan(pp1,fig,ax, fidelity = fid, pt =0)
 
-	#	----------------------------------------------------------
-	# 2) Initialize the estimate for the parameters (by zero or
-	# 		by using odometry data)
-	x = np.zeros([3,1]) #[ t_x, t_y, theta] }we want to optimize a single euler angle rather than a matrix
+# 	#	----------------------------------------------------------
+# 	# 2) Initialize the estimate for the parameters (by zero or
+# 	# 		by using odometry data)
+# 	x = np.zeros([3,1]) #[ t_x, t_y, theta] }we want to optimize a single euler angle rather than a matrix
 
-	#get 2d coords of centers of each ellipse from E1
-	ctr = np.zeros([len(E1),2])
-	for idx, c in enumerate(E1):
-		ctr[idx,:] = c[0]
+# 	#get 2d coords of centers of each ellipse from E1
+# 	ctr = np.zeros([len(E1),2])
+# 	for idx, c in enumerate(E1):
+# 		ctr[idx,:] = c[0]
 
-	#debug- draw centers of ellipses from 1st scan
-	ax.plot(ctr[:,0], ctr[:,1], 'ko')
+# 	#debug- draw centers of ellipses from 1st scan
+# 	ax.plot(ctr[:,0], ctr[:,1], 'ko')
 
-	results = []
+# 	results = []
 
-	for cycle in range(num_cycles):
-		# print(" ------------------ cycle: ", cycle, "----------------------" )
+# 	for cycle in range(num_cycles):
+# 		# print(" ------------------ cycle: ", cycle, "----------------------" )
 
-		scores = np.zeros(4)
-		grad = np.zeros([3,1])
-		grad_step_xy = 0.1 #how far we step in x and y when calculating gradients
-		grad_step_theta = 0.01
-		stepsize = 0.05 #how fast we move in gradient descent
+# 		scores = np.zeros(4)
+# 		grad = np.zeros([3,1])
+# 		grad_step_xy = 0.1 #how far we step in x and y when calculating gradients
+# 		grad_step_theta = 0.01
+# 		stepsize = 0.05 #how fast we move in gradient descent
 
-		#compute score for baseline and differential changes in each parameter
-		for param in range(4): 
+# 		#compute score for baseline and differential changes in each parameter
+# 		for param in range(4): 
 
-			#get baseline
-			x_temp = np.zeros(np.shape(x))
-			x_temp[:]  = x[:]
+# 			#get baseline
+# 			x_temp = np.zeros(np.shape(x))
+# 			x_temp[:]  = x[:]
 
-			#differentially change x and y one at a time
-			if param == 0:
-				x_temp[0] += grad_step_xy
-			if param == 1:
-				x_temp[1] += grad_step_xy
-			if param == 2:
-				x_temp[2] += grad_step_theta
-			# print("x_temp ",x_temp)
+# 			#differentially change x and y one at a time
+# 			if param == 0:
+# 				x_temp[0] += grad_step_xy
+# 			if param == 1:
+# 				x_temp[1] += grad_step_xy
+# 			if param == 2:
+# 				x_temp[2] += grad_step_theta
+# 			# print("x_temp ",x_temp)
 
-			# 	---------------------------------------------------------
-			# 3) For each sample of the second scan: Map the
-			# 		reconstructed 2D point into the coordinate frame of
-			# 		the first scan according to the parameters.
+# 			# 	---------------------------------------------------------
+# 			# 3) For each sample of the second scan: Map the
+# 			# 		reconstructed 2D point into the coordinate frame of
+# 			# 		the first scan according to the parameters.
 
-			#apply transformation
-			t_temp = x_temp[:2]
-			rot_temp = R(x_temp[2]) 
-			P_temp = rot_temp.dot(pp2.T) + t_temp #P_temp is P_corrected with differential changes to each param in x
+# 			#apply transformation
+# 			t_temp = x_temp[:2]
+# 			rot_temp = R(x_temp[2]) 
+# 			P_temp = rot_temp.dot(pp2.T) + t_temp #P_temp is P_corrected with differential changes to each param in x
 
-			#to debug correspondences
-			# if (cycle == num_cycles - 1) and (param == 3):
-			# 	dc = True
-			# else:
-			# 	dc = False 
-			dc = False
-			correspondences = get_correspondence(P_temp,ctr.T,fig,ax, draw = dc)
-			# print("c ", correspondences[0], np.shape(correspondences))
+# 			#to debug correspondences
+# 			# if (cycle == num_cycles - 1) and (param == 3):
+# 			# 	dc = True
+# 			# else:
+# 			# 	dc = False 
+# 			dc = False
+# 			correspondences = get_correspondence(P_temp,ctr.T,fig,ax, draw = dc)
+# 			# print("c ", correspondences[0], np.shape(correspondences))
 
-			#	----------------------------------------------------------
-			#4) Determine the corresponding normal distributions
-			# 		for each mapped point.
-			score = 0
-			for index, i in enumerate(correspondences[0]): 
+# 			#	----------------------------------------------------------
+# 			#4) Determine the corresponding normal distributions
+# 			# 		for each mapped point.
+# 			score = 0
+# 			for index, i in enumerate(correspondences[0]): 
 
-				mu = E1[int(i)][0][:,None]  	#center of corresponding point cloud from 1st scan (q_i in Biber paper)
-				# print("mu", mu, np.shape(mu))
-				# print("x_i", P_temp[:,index])
-				sigma = E1[int(i)][1]	#associated covariance matrix
+# 				mu = E1[int(i)][0][:,None]  	#center of corresponding point cloud from 1st scan (q_i in Biber paper)
+# 				# print("mu", mu, np.shape(mu))
+# 				# print("x_i", P_temp[:,index])
+# 				sigma = E1[int(i)][1]	#associated covariance matrix
 
-				eig = np.linalg.eig(sigma)
-				eigenval = eig[0]
-				eigenvec = eig[1]
+# 				eig = np.linalg.eig(sigma)
+# 				eigenval = eig[0]
+# 				eigenvec = eig[1]
 
-				# Matt's z-score method (not a good idea)-------------------------------------------
-				# #get major and minor axis length of corresponding error ellipse
-				# #debug - figure out which is which
-				major = np.sqrt(eigenval[1]) + 3 #debug: add minimum size to each ellipse (without this errors explode...)
-				minor = np.sqrt(eigenval[0]) + 3
+# 				# Matt's z-score method (not a good idea)-------------------------------------------
+# 				# #get major and minor axis length of corresponding error ellipse
+# 				# #debug - figure out which is which
+# 				major = np.sqrt(eigenval[1]) + 3 #debug: add minimum size to each ellipse (without this errors explode...)
+# 				minor = np.sqrt(eigenval[0]) + 3
 
-				#get rotation of ellipse
-				theta_temp = np.arcsin(eigenvec[0,1]/eigenvec[0,0])
+# 				#get rotation of ellipse
+# 				theta_temp = np.arcsin(eigenvec[0,1]/eigenvec[0,0])
 
-				#rotate point about origin so that axis of ellipse can be aligned with x,y axis
-				rot_temp = R(theta_temp)
-				pt_rot = rot_temp.dot(pp2[index]) #DEBUG -THIS IS THE PROBLEM??!!!
-				# pt_rot = rot_temp.dot(P[:,index])
+# 				#rotate point about origin so that axis of ellipse can be aligned with x,y axis
+# 				rot_temp = R(theta_temp)
+# 				pt_rot = rot_temp.dot(pp2[index]) #DEBUG -THIS IS THE PROBLEM??!!!
+# 				# pt_rot = rot_temp.dot(P[:,index])
 
-				#figure out how much I need to scale 1STD ellipse to reach point
-				ratio = major/minor			
-				b = np.sqrt( (pt_rot[0]**2)/(ratio**2) + pt_rot[1]**2 )
-				a = ratio*b
+# 				#figure out how much I need to scale 1STD ellipse to reach point
+# 				ratio = major/minor			
+# 				b = np.sqrt( (pt_rot[0]**2)/(ratio**2) + pt_rot[1]**2 )
+# 				a = ratio*b
 
-				#use z_score as error
-				z_score = (a / major)#**2 #number of standard deviations point is from closest ellipse center
+# 				#use z_score as error
+# 				z_score = (a / major)#**2 #number of standard deviations point is from closest ellipse center
 
-				#this works(ish) but is not what is actually in the Biber paper
-				score += z_score #keep track of overall scores
-
-
-				#Biber paper: --------------------------------------------------------------
-				# score += -e^( (q.T)(cov)(q) / 2 )
-				# 		where: q = distance between x_i and corresponding NDT point  
-				# 			   ** assume cov is positive definate
-
-				#reorient each point along the axis of the Normal Distribution ellipse(????) ------------------
-				# ang_from_ell_ax = np.arcsin(eigenvec[0,1]/eigenvec[0,0])
-				# rot_to_ell_frame = R(ang_from_ell_ax)	
-				# #NOTE: x is [x y theta] x' is a point (the notation here is really bad)
-				# x_prime = rot_to_ell_frame.dot(P_temp[:,index])[:,None] #Here P represents transformed coord system?
-				# # print("before: ", P_temp[:,index], "\n rotated to ell frame: ", x_prime) #makes sense...
-				# score += np.exp( (-( x_prime - mu ).T.dot(np.linalg.pinv(sigma)).dot(x_prime - mu) )/2 ) #-----
-
-				# print(P_temp[:,index][:,None])
-
-				#without reorientation
-				# score += np.exp( (-( P_temp[:,index][:,None] - mu ).T.dot(np.linalg.pinv(sigma)).dot(P_temp[:,index][:,None] - mu) )/2 ) #-----				
+# 				#this works(ish) but is not what is actually in the Biber paper
+# 				score += z_score #keep track of overall scores
 
 
-			#	-----------------------------------------------------------------
-			# 5) The score for the parameters is determined by
-			# 		evaluating the distribution for each mapped point
-			# 		and summing the result.
+# 				#Biber paper: --------------------------------------------------------------
+# 				# score += -e^( (q.T)(cov)(q) / 2 )
+# 				# 		where: q = distance between x_i and corresponding NDT point  
+# 				# 			   ** assume cov is positive definate
 
-			# print(" score: ", score, "param ", param)# "x_temp ", x_temp)
-			scores[param] = score
-		scores = -scores
-		print("scores: ",scores)
+# 				#reorient each point along the axis of the Normal Distribution ellipse(????) ------------------
+# 				# ang_from_ell_ax = np.arcsin(eigenvec[0,1]/eigenvec[0,0])
+# 				# rot_to_ell_frame = R(ang_from_ell_ax)	
+# 				# #NOTE: x is [x y theta] x' is a point (the notation here is really bad)
+# 				# x_prime = rot_to_ell_frame.dot(P_temp[:,index])[:,None] #Here P represents transformed coord system?
+# 				# # print("before: ", P_temp[:,index], "\n rotated to ell frame: ", x_prime) #makes sense...
+# 				# score += np.exp( (-( x_prime - mu ).T.dot(np.linalg.pinv(sigma)).dot(x_prime - mu) )/2 ) #-----
 
-		#	-----------------------------------------------------------------
-		#6) Calculate a new parameter estimate by trying to
-		# 		optimize the score. This is done by performing one
-		# 		step of Newton’s Algorithm.
+# 				# print(P_temp[:,index][:,None])
 
-		# print("score: ",scores[-1])
-		results = np.append(results, score)
+# 				#without reorientation
+# 				# score += np.exp( (-( P_temp[:,index][:,None] - mu ).T.dot(np.linalg.pinv(sigma)).dot(P_temp[:,index][:,None] - mu) )/2 ) #-----				
 
-		# Newton's method
-		# x1 -> x0 + f(x0)/f'x(0)
-		dx = np.zeros(3)
-		dx[0] = scores[0] / ((scores[0] - scores[3])/grad_step_xy) 
-		dx[1] = scores[1] / ((scores[1] - scores[3])/grad_step_xy) 
-		dx[2] = scores[2] / ((scores[2] - scores[3])/ grad_step_theta)
-		#NOTE: /0 error here means that changing x results in no score change
+
+# 			#	-----------------------------------------------------------------
+# 			# 5) The score for the parameters is determined by
+# 			# 		evaluating the distribution for each mapped point
+# 			# 		and summing the result.
+
+# 			# print(" score: ", score, "param ", param)# "x_temp ", x_temp)
+# 			scores[param] = score
+# 		scores = -scores
+# 		print("scores: ",scores)
+
+# 		#	-----------------------------------------------------------------
+# 		#6) Calculate a new parameter estimate by trying to
+# 		# 		optimize the score. This is done by performing one
+# 		# 		step of Newton’s Algorithm.
+
+# 		# print("score: ",scores[-1])
+# 		results = np.append(results, score)
+
+# 		# Newton's method
+# 		# x1 -> x0 + f(x0)/f'x(0)
+# 		dx = np.zeros(3)
+# 		dx[0] = scores[0] / ((scores[0] - scores[3])/grad_step_xy) 
+# 		dx[1] = scores[1] / ((scores[1] - scores[3])/grad_step_xy) 
+# 		dx[2] = scores[2] / ((scores[2] - scores[3])/ grad_step_theta)
+# 		#NOTE: /0 error here means that changing x results in no score change
 		
-		print("dx ",dx)
+# 		print("dx ",dx)
 
-		x += stepsize*dx[:,None]
-		print("x = ",x)
+# 		x += stepsize*dx[:,None]
+# 		print("x = ",x)
 
-		#DEBUG
+# 		#DEBUG
 
-		#draw all points progressing through transformation
-		rot = R(x[2])
-		t = x[0:2]
-		P_corrected = rot.dot(pp2.T) + t
-		P_corrected = P_corrected.T
-		ax.plot(P_corrected.T[0,:], P_corrected.T[1,:], color = (1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),0.025), ls = '', marker = '.', markersize = 20)
+# 		#draw all points progressing through transformation
+# 		rot = R(x[2])
+# 		t = x[0:2]
+# 		P_corrected = rot.dot(pp2.T) + t
+# 		P_corrected = P_corrected.T
+# 		ax.plot(P_corrected.T[0,:], P_corrected.T[1,:], color = (1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),1-(cycle+1)/(num_cycles+1),0.025), ls = '', marker = '.', markersize = 20)
 
 
-	#draw transformed point set
-	rot_final = R(x[2])
-	t_final = x[:2]
+# 	#draw transformed point set
+# 	rot_final = R(x[2])
+# 	t_final = x[:2]
 
-	P_corrected = rot_final.dot(pp2.T) + t_final
-	ax.plot(P_corrected[0,:], P_corrected[1,:], color = (1,0,0,0.0625), ls = '', marker = '.', markersize = 15)
+# 	P_corrected = rot_final.dot(pp2.T) + t_final
+# 	ax.plot(P_corrected[0,:], P_corrected[1,:], color = (1,0,0,0.0625), ls = '', marker = '.', markersize = 15)
 		
-	return x[2], t_final, results
+# 	return x[2], t_final, results
