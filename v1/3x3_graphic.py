7dc977da3f047c0e7eb88d607bb6d145f757e134
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2021-09-02 17:19:07 -0400

    updated fig1 per Jason's feedback

--- v1/3x3_graphic.py
+++ v1/3x3_graphic.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from utils import *
+import time
 #for generating scans
 from player import Player 
 from game import game
@@ -16,16 +17,17 @@ high_noise = 15
 low_noise = 0.1
 FOV = 135 #for wall
 fid = 4
-minpts = 3
+minpts = 15
+fov_fid = 200
 
 fig, ax = plt.subplots(nrows = 3, ncols = 3)
 fig.tight_layout() #meh
-fig.subplots_adjust(left = 0.01, right = 0.99, hspace = 0.01, wspace = 0.01) #slightly better
+fig.subplots_adjust(left = 0.1, right = 0.9, hspace = 0.03, wspace = 0.03) #slightly better
 
 #set labels on top
-ax[0,0].set_title("Road Sign")
-ax[0,1].set_title("Car")
-ax[0,2].set_title("Extended Wall")
+ax[0,0].set_title("Small Object")
+ax[0,1].set_title("Voxel Sized Object")
+ax[0,2].set_title("Object Extends Over Multiple Voxels")
 #show labels on side
 ax[0,0].set_ylabel("Actual Object (above)")
 ax[1,0].set_ylabel("Noise Free Scan")
@@ -62,19 +64,21 @@ for i in range(3):
 
 
 # show base objects -------------------------------------------
-sign_file = "assets/sign.png"
+# sign_file = "assets/sign.png"
+sign_file = "assets/human.png"
 sign_img = cv2.imread(sign_file)
 ax[0,0].imshow(sign_img,cmap="gray", interpolation = 'bicubic')
 ax[0,0].set_xlim(0,800)
 ax[0,0].set_ylim(800,0)
 
-car_file = "assets/car.png"
+# car_file = "assets/car.png"
+car_file = 'assets/element.png'
 car_img = cv2.imread(car_file)
 ax[0,1].imshow(car_img,cmap="gray", interpolation = 'bicubic')
 ax[0,1].set_xlim(0,800)
 ax[0,1].set_ylim(800,0)
 
-wall_file = "assets/wall2.png"
+wall_file = "assets/wall.png"
 wall_img = cv2.imread(wall_file)
 ax[0,2].imshow(wall_img,cmap="gray", interpolation = 'bicubic')
 ax[0,2].set_xlim(0,800)
@@ -85,7 +89,7 @@ ax[0,2].set_ylim(800,0)
 #sign
 g0 = game(fig, ax[0,0], sign_img)#cv2.flip(sign_img,0))
 g0.p.FOV = np.deg2rad(FOV)
-g0.p.fovfid = 100
+g0.p.fovfid = fov_fid
 g0.p.pos = np.array([400,800])
 g0.p.heading = np.pi #0 #looking straight up
 #high noise scan - do first
@@ -106,7 +110,7 @@ ax[1,0].set_ylim(0,800)
 #car
 g1 = game(fig, ax[0,1], car_img)#cv2.flip(car_img,0))
 g1.p.FOV = np.deg2rad(FOV)
-g1.p.fovfid = 100
+g1.p.fovfid = fov_fid
 g1.p.pos = np.array([400,800])
 g1.p.heading = np.pi #0 #looking straight up
 #high noise scan
@@ -128,7 +132,7 @@ ax[1,1].set_ylim(0,800)
 #wall
 g2 = game(fig, ax[0,2], wall_img)#cv2.flip(wall_img,0)
 g2.p.FOV = np.deg2rad(FOV)
-g2.p.fovfid = 100
+g2.p.fovfid = fov_fid
 # g2.p.pos = np.array([1200,800])
 g2.p.pos = np.array([400,800])
 g2.p.heading = np.pi #0 #looking straight up
@@ -155,16 +159,16 @@ ax[1,2].set_ylim(0,800)
 #generate covariance ellipses --------------------------------
 
 #sign
-subdivide_scan(pp0_low[np.squeeze(np.argwhere(g0hit))], fig, ax[1,0], fidelity = fid, min_num_pts = minpts, flag = True)
-subdivide_scan(pp0_high[np.squeeze(np.argwhere(g0hit2))], fig, ax[2,0], fidelity = fid, min_num_pts = minpts, flag = True)
+subdivide_scan(pp0_low[np.squeeze(np.argwhere(g0hit))], fig, ax[1,0], fidelity = fid, min_num_pts = 5, flag = True)
+subdivide_scan(pp0_high[np.squeeze(np.argwhere(g0hit2))], fig, ax[2,0], fidelity = fid, min_num_pts = 5, flag = True)
 
 #car
 subdivide_scan(pp1_low[np.squeeze(np.argwhere(g1hit))], fig, ax[1,1], fidelity = fid, min_num_pts = minpts, flag = True )
 subdivide_scan(pp1_high[np.squeeze(np.argwhere(g1hit2))], fig, ax[2,1], fidelity = fid, min_num_pts = minpts, flag = True)
 
 #wall
-subdivide_scan(pp2_low[np.squeeze(np.argwhere(g2hit))], fig, ax[1,2], fidelity = fid, min_num_pts = 10, flag = True)
-subdivide_scan(pp2_high[np.squeeze(np.argwhere(g2hit2))], fig, ax[2,2], fidelity = fid, min_num_pts = 10, flag = True)
+subdivide_scan(pp2_low[np.squeeze(np.argwhere(g2hit))], fig, ax[1,2], fidelity = fid, min_num_pts = minpts, flag = True)
+subdivide_scan(pp2_high[np.squeeze(np.argwhere(g2hit2))], fig, ax[2,2], fidelity = fid, min_num_pts = minpts, flag = True)
 
 # ------------------------------------------------------------
 # ax[2,2].grid(color = 'r', linestyle = '-', linewidth = 2, zorder = 1)
@@ -173,3 +177,15 @@ subdivide_scan(pp2_high[np.squeeze(np.argwhere(g2hit2))], fig, ax[2,2], fidelity
 
 plt.pause(0.01)
 plt.show()
+
+file_location = "assets/3x3v2.png"
+fig.set_size_inches(10,10)
+fig.savefig(file_location)
+time.sleep(1)
+
+temp = cv2.imread(file_location, cv2.IMREAD_GRAYSCALE)
+# gray = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grayscale', temp)
+# time.sleep(1)
+cv2.waitKey(3000)
+cv2.imwrite(file_location, temp)
