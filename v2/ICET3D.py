more debugging viz- flipped sign in Exz Eyz when calculating sigma, seems to fix issues

v2/ICET3D.py
@@ -8,16 +8,24 @@ import tensorflow_probability as tfp
 import time
 from utils import *
 
-
+#Optimization:
 #TODO: 	figure out why memory usage is increasing after each loop
 			# https://stackoverflow.com/questions/44825360/tensorflows-memory-cost-gradually-increasing-in-very-simple-for-loop/44825824
 
+#Viz:
 #TODO: 	remove past iterations of point cloud in viz
 #TODO: 	add slider to allow selection of iterations
 #			generate every <x> beforehand 
+#TODO:	visualize L1
+
+#Algorithm: 
+#TODO: 	figure out why I need to make angs[0] and angs[1] negative
+#TODO:	Make sure voxels with insufficeint number of points are getting ignored
+#TODO:	Implement cutoff threshold values for L1
+
 
 def ICET3D(pp1, pp2, plt, bounds, fid, test_dataset = False,  draw = False, 
-	       num_cycles = 5, min_num_pts = 30, draw_grid = False, draw_ell = True, 
+	       num_cycles = 5, min_num_pts = 50, draw_grid = False, draw_ell = True, 
 	       draw_corr = False, CM = "voxel"):
 
 	"""3D implementation of ICET algorithm using TensorFlow library
@@ -40,6 +48,9 @@ def ICET3D(pp1, pp2, plt, bounds, fid, test_dataset = False,  draw = False,
 	npts1 = E1[2]
 	disp1 = E1[3]
 
+	#TODO: DEBUG -> I think there is a mismatch between npts1 and its corresponding elements in sigma1, etc.
+	# print("\n npts1 \n", npts1)
+
 	# ignore data from unused voxels
 	nonzero_idx1 = tf.where(tf.math.reduce_sum(mu1, axis = 1) != 0)
 	y0 = tf.squeeze(tf.gather(mu1,nonzero_idx1))
@@ -54,9 +65,6 @@ def ICET3D(pp1, pp2, plt, bounds, fid, test_dataset = False,  draw = False,
 
 	# calculte overly extended directions for each remaining distribution  
 	U, L = get_U_and_L(sigma1, bounds, fid)
-
-	#TODO: visualize U and L
-	#		replace ellipsoids with arrows in compact axis directions??
 
 	#init solution vector x
 	x = tf.zeros(6) #[x, y, z, phi, theta, psi].T
@@ -205,12 +213,12 @@ def ICET3D(pp1, pp2, plt, bounds, fid, test_dataset = False,  draw = False,
 		print("\n x \n", x)
 
 		#transform 2nd scan by x
-		t = x[:3]
-		rot = R_tf(x[3:])
+		# t = x[:3]
+		# rot = R_tf(x[3:])
 
 		# # DEBUG: only consider yaw transformations
-		# t = tf.constant([0., 0., 0.])
-		# rot = R_tf(tf.constant([0., 0., x[5].numpy()]))	
+		t = tf.constant([0., 0., 0.])
+		rot = R_tf(tf.constant([0., 0., x[5].numpy()]))	
 
 		# print("\n t, rot \n", t, "\n", rot)
 

v2/main.py
@@ -18,8 +18,16 @@ from ICET3D import ICET3D
 #NOTE: Out of Memory Error comes from too high fidelity/ pts in cloud tensor --> 100x100x2x120,000 > 2gb
 
 
-plt = Plotter(N=1, axes=1, bg = (0.1,0.1,0.1), bg2 = (0.3,0.3,0.3),  interactive=True)
+nc = 2 	 #number of cycles
+mnp = 25 #minimum number of points per voxel
+D = True #draw sim
+DG = False #draw grid
+DE = True #draw ellipsoids
+DC = True #draw correspondences
+TD = True #use test dataset
+CM = "voxel" #correspondence method, "voxel" or "NN"
 
+plt = Plotter(N=1, axes=1, bg = (0.1,0.1,0.1), bg2 = (0.3,0.3,0.3),  interactive=True)
 basedir = 'C:/kitti/'
 date = '2011_09_26'
 drive = '0005'
@@ -32,23 +40,13 @@ velo2 = dataset.get_velo(2) # Each scan is a Nx4 array of [x,y,z,reflectance]
 cloud2 = velo2[:,:3]
 cloud2_tensor = tf.convert_to_tensor(cloud2, np.float32)
 
-nc = 5
-mnp = 15
-npts = 100000
-D = True #draw sim
-DG = False #draw grid
-DE = True #draw ellipsoids
-DC = True #draw correspondences
-TD = True #use test dataset
-CM = "voxel" #correspondence method, "voxel" or "NN"
-
 start = time.time()
-
 
 # #use whole point set
 # #---------------------------------------------------------------------------------
 # f = tf.constant([50,50,2]) #fidelity in x, y, z # < 5s
 # lim = tf.constant([-100.,100.,-100.,100.,-10.,10.]) #needs to encompass every point
+# npts = 100000
 # Q, x_hist = ICET3D(cloud1_tensor[:npts], cloud2_tensor[:npts], plt, bounds = lim, 
 #            fid = f, num_cycles = nc , min_num_pts = mnp, draw = D, draw_grid = DG, 
 #            draw_ell = DE, draw_corr = DC)
@@ -58,7 +56,7 @@ start = time.time()
 #just consider small section of image where there are easily identifiable features:
 #----------------------------------------------------------------------------------
 limtest = tf.constant([-20.,0.,-20.,0.,-1.5,1.5])
-f = tf.constant([10,10,10])
+f = tf.constant([20,20,20])
 # cloud1_tensor = tf.squeeze(tf.gather(cloud1_tensor, tf.where( (cloud1_tensor[:,0] > limtest[0]))))	#only works one cond at a time
 cloud1_tensor = tf.squeeze(tf.gather(cloud1_tensor, tf.where( tf.math.reduce_all(tf.concat( (
 	(cloud1_tensor[:,0] > limtest[0])[:,None], 

v2/utils.py
@@ -491,8 +491,8 @@ def subdivide_scan_tf(cloud_tensor, plt, bounds = tf.constant([-50.,50.,-50.,50.
 
 	# E_xy = mean((xpts-mux)(ypts-muy))
 	E_xy = tf.reduce_sum( ((vox_with_zeros[:,:,0] - mu[:,0][:,None])*(vox_with_zeros[:,:,1] - mu[:,1][:,None]))*mask , axis = 1)/ tf.cast(sizes_updated, tf.float32)
-	E_xz = tf.reduce_sum( ((vox_with_zeros[:,:,0] - mu[:,0][:,None])*(vox_with_zeros[:,:,2] - mu[:,2][:,None]))*mask , axis = 1)/ tf.cast(sizes_updated, tf.float32)
-	E_yz = tf.reduce_sum( ((vox_with_zeros[:,:,1] - mu[:,1][:,None])*(vox_with_zeros[:,:,2] - mu[:,2][:,None]))*mask , axis = 1)/ tf.cast(sizes_updated, tf.float32)
+	E_xz = -tf.reduce_sum( ((vox_with_zeros[:,:,0] - mu[:,0][:,None])*(vox_with_zeros[:,:,2] - mu[:,2][:,None]))*mask , axis = 1)/ tf.cast(sizes_updated, tf.float32)
+	E_yz = -tf.reduce_sum( ((vox_with_zeros[:,:,1] - mu[:,1][:,None])*(vox_with_zeros[:,:,2] - mu[:,2][:,None]))*mask , axis = 1)/ tf.cast(sizes_updated, tf.float32)
 	
 	# [3,3,N]
 	# sigma = tf.Variable([[std_x, E_xy, E_xz],
@@ -504,6 +504,7 @@ def subdivide_scan_tf(cloud_tensor, plt, bounds = tf.constant([-50.,50.,-50.,50.
 						 E_xy, std_y, E_yz,
 						 E_xz, E_yz, std_z]) 
 	sigma = tf.reshape(tf.transpose(sigma), (tf.shape(sigma)[1] ,3,3))
+	# print("sigma \n", sigma)
 
 	#get rid of any nan values in sigma
 
@@ -525,6 +526,8 @@ def subdivide_scan_tf(cloud_tensor, plt, bounds = tf.constant([-50.,50.,-50.,50.
 def make_scene(plt, disp, E, color, draw_grid = False, draw_ell = True, fid = None, bounds =None):
 
 	"""draw distribution ellipses from E
+	E = [mus, sigmas, sizes, disp]
+
 	 called by subdivide_scan_tf() """
 
 	mu = E[0]
@@ -857,9 +860,12 @@ class Ell(Mesh):
         t = vtk.vtkTransform()
         t.PostMultiply()
         t.Scale(l1, l2, l3)
+
+        #needed theta and angle to be negative before messing with E_xz, E_yz...
         t.RotateZ(np.rad2deg(phi))
         t.RotateY(np.rad2deg(theta))
         t.RotateX(np.rad2deg(angle))
+        
         tf = vtk.vtkTransformPolyDataFilter()
         tf.SetInputData(elliSource.GetOutput())
         tf.SetTransform(t)
@@ -881,7 +887,7 @@ def generate_test_dataset():
 
 	#TODO: take in transformation from pp1 to pp2
 
-	bounds = tf.constant ([-120.,120.,-120.,120.,-100,100])
+	bounds = tf.constant ([-150.,150.,-150.,150.,-150,150])
 	x = tf.constant([0., 0., 0., 0., 0., -0.1]) # yaw
 	# x = tf.constant([0., 0., 0., 0., 0., 0.])
 