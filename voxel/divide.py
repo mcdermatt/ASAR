b767cdce40e4ba7b098ca59aa09da2fde0cbc99a
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2022-01-24 13:17:50 -0500

    added fit gausian method

--- voxel/divide.py
+++ voxel/divide.py
@@ -12,6 +12,8 @@ class divide():
 
 	def __init__(self, fig, ax, cloud = None):
 
+		self.minNumPts = 3
+
 		self.fig = fig
 		self.ax = ax
 		self.patches = []
@@ -27,34 +29,73 @@ class divide():
 
 		self.drawNodes()
 
-		self.drawVoxel(4)
 
-		self.findPtsInVox(4)
+		# self.findPtsInVox(4)
 
 		c = self.getCentroid(4)
 		self.ax.plot(c[0],c[1],'bx')
 
+		self.main()
 
-		p = PatchCollection(self.patches, alpha=0.4)
-		self.ax.add_collection(p)
 		plt.show()
 
+
+	def main(self):
+		""" Main loop: iteratively adjusts placement of nodes to optimize a score function """
+
+		numIter = 1
+		numVox = (self.node_fidelity-1)**2
+
+		for i in range(numIter):
+			for j in range(numVox):
+
+				pts = self.findPtsInVox(j)
+
+				self.drawVoxel(j, npts = np.shape(pts)[0])
+
+				#only consider voxels with sufficient number of points
+
+				if np.shape(pts)[0] > self.minNumPts:
+					mu, sigma = self.fit_gaussian(pts)
+					# print(sigma)
+
+
+
+	def fit_gaussian(self, pts, draw = True):
+		""" calaculates covaraince of group of points """
+
+		mu_x = np.mean(pts[:,0])
+		mu_y = np.mean(pts[:,1])
+		mu = np.array([mu_x, mu_y])
+
+		std_x = np.sum( (pts[:,0] - mu_x)*(pts[:,0] - mu_x) ) / (np.shape(pts)[0] - 1 )
+		std_y = np.sum( (pts[:,1] - mu_y)*(pts[:,1] - mu_y) ) / (np.shape(pts)[0] - 1 )
+		std_xy= np.sum((pts[:,0] - mu_x)*(pts[:,1] - mu_y) ) / (np.shape(pts)[0] - 1)
+		sigma = np.array([[std_x,  std_xy],
+						  [std_xy, std_y]])
+
+
+		if draw == True:
+			#draw ellipse			
+			pass
+
+		return(mu, sigma)
 
 	def initNodes(self):
 		""" set initial positions of nodes for voxel grid """
 
 		node_boundaries = np.array([-10, 10, -10, 10]) #[minx, maxx, miny, maxy]
-		node_fidelity = 4 #this many nodes in x and y 
+		self.node_fidelity = 4 #this many nodes in x and y 
 
-		self.Nodes = np.zeros([node_fidelity**2,2])
+		self.Nodes = np.zeros([self.node_fidelity**2,2])
 
-		self.Nodes[:,0] = np.tile(np.linspace(node_boundaries[0], node_boundaries[1], node_fidelity), node_fidelity)
+		self.Nodes[:,0] = np.tile(np.linspace(node_boundaries[0], node_boundaries[1], self.node_fidelity), self.node_fidelity)
 
-		ypos = np.linspace(node_boundaries[3], node_boundaries[2], node_fidelity)
-		for i in range(1,node_fidelity+1):
-			self.Nodes[(i-1)*node_fidelity:i*node_fidelity,1] = ypos[i-1]
+		ypos = np.linspace(node_boundaries[3], node_boundaries[2], self.node_fidelity)
+		for i in range(1,self.node_fidelity+1):
+			self.Nodes[(i-1)*self.node_fidelity:i*self.node_fidelity,1] = ypos[i-1]
 
-		print("created grid with", (node_fidelity-1)**2, "voxels")
+		print("created grid with", (self.node_fidelity-1)**2, "voxels")
 
 
 	def drawNodes(self):
@@ -63,12 +104,12 @@ class divide():
 	def getCorners(self,voxid):
 		""" gets corner nodes bounding voxel # voxid """
 
-		node_fidelity = 4  
+		self.node_fidelity = 4  
 
-		tl = voxid + voxid//(node_fidelity - 1) #top left
+		tl = voxid + voxid//(self.node_fidelity - 1) #top left
 		tr = tl+1
-		br = tr + node_fidelity
-		bl = tl + node_fidelity
+		br = tr + self.node_fidelity
+		bl = tl + self.node_fidelity
 
 		corners = np.array([self.Nodes[tl,:],
 							self.Nodes[tr,:], 
@@ -76,19 +117,28 @@ class divide():
 							self.Nodes[bl,:]])
 		return corners
 
-	def drawVoxel(self, voxid):
+	def drawVoxel(self, voxid, npts = -1):
 		""" draws polygon using corners"""
 		corners = self.getCorners(voxid)
 
-		poly = Polygon(corners, True)
+		poly = Polygon(corners, closed = True)
+
+		self.patches = []
 		self.patches.append(poly)
+		p = PatchCollection(self.patches, alpha=0.4)
+
+		if npts != -1:
+			p.set_color([1 - npts/(np.shape(self.cloud)[0]) ,1 - npts/(np.shape(self.cloud)[0]) ,1 ])
+
+		self.ax.add_collection(p)
 
 	def genDemoCloud(self):
 		"""generate simple structured point cloud for testing """
 
-		self.cloud = np.random.randn(10,2)
-		self.cloud[:,0] += np.linspace(-3,3,10)
-		self.cloud[:,1] += np.linspace(1,4,10)
+		npts = 50
+		self.cloud = 0.5*np.random.randn(npts,2)
+		self.cloud[:,0] += np.linspace(-8,8,npts)
+		self.cloud[:,1] += np.linspace(1,4,npts)
 
 
 		self.ax.plot(self.cloud[:,0], self.cloud[:,1], 'r.')
@@ -132,7 +182,5 @@ class divide():
 
 		#centroid of the combined quadralateral is the weighted sum of the two constituant triangles
 		c = center1*(area1/(area1+area2)) + center2*(area2/(area1+ area2))
-
-		# print(c)
 
 		return c
