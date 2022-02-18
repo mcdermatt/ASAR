d269378a27c79e9e0da3fed2b903827974e30abe
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2022-02-18 15:57:27 -0500

    identified widest corners in occupied voxels

--- voxel/scene.py
+++ voxel/scene.py
@@ -15,15 +15,80 @@ class scene():
 		self.numvox = (self.fid+1)*(self.fid + 1)*(self.fid + 1) #number of voxels
 		self.wire = True #draw cells as wireframe
 
-		self.plt = Plotter(N = 1, axes = 0, bg = (1, 1, 1), interactive = True)
+		self.plt = Plotter(N = 1, axes = 4, bg = (1, 1, 1), interactive = True)
 		self.disp = []
 
 		self.occupancy_grid(draw = False)
+		# self.cull()
 
 		self.draw_cloud()
 		self.draw_car()
 		self.plt.show(self.disp, "3D Occlusion Culling Test")
 
+
+	def cull(self):
+		""" culls all voxels occluded from POV of observer """
+		
+		#loop through each occupied voxel
+		for o in self.occupied:
+
+			#get outside corners of cell 
+			voxctr = np.squeeze(self.grid[o])
+			corners = np.ones([8,3]) * voxctr
+			corners[:,0] -= self.cw/2			
+			corners[2:4,0] += self.cw
+			corners[6:,0] += self.cw
+			corners[:,1] -= self.cw/2			
+			corners[0:-1:2,1] += self.cw
+			corners[:4,2] += self.cw/2
+			corners[4:,2] -= self.cw/2
+			# print(corners)
+			# print(voxctr)
+
+			#get angle to center of cell
+			#rotation about vertical axis
+			# theta = np.sin(voxctr[0]/voxctr[1]) #wrong?
+			theta = np.arctan2(voxctr[1],voxctr[0])
+			if np.isnan(theta) == True:
+				theata = 0
+			# print("theta", theta)
+			#rotation up/down
+			# phi = 
+			dthetamax = 0
+			dthetamin = 0
+			for i in range(8):			#loop through each corner
+				#find corners with largest horizonatal rotation band
+				# theta_i = np.sin(corners[i,0]/corners[i,1])
+				theta_i = np.arctan2(corners[i,1],corners[i,0])
+				# print("theta_i", theta_i)
+				dtheta = theta_i - theta
+				# print("dtheta", dtheta)
+				if dtheta > dthetamax:
+					dthetamax = dtheta
+					thetamax = i
+				if dtheta < dthetamin:
+					dthetamin = dtheta
+					thetamin = i				
+
+				#find corners with largest vertical rotation band			
+					#...
+
+			L_theta_min = shapes.Line(np.array([0,0,0]), corners[thetamin], lw = 4, c ='b')
+			L_theta_max = shapes.Line(np.array([0,0,0]), corners[thetamax], lw = 4, c='b')
+			self.disp.append(L_theta_min)
+			self.disp.append(L_theta_max)
+
+			corn = Points(corners, c = [0.5,0.9,0.5], r = 10)
+			self.disp.append(corn)
+
+			#find grid centers that fall between thetamin and thetamax
+			grid_thetas = np.arctan2(self.grid[:,1], self.grid[:,0])
+			between = np.asarray(np.where(np.array([ grid_thetas < thetamax,
+										  grid_thetas > thetamin
+											]).all(axis = 0) == True)).T
+			# print(between)
+			for b in between:
+				self.draw_cell(b, wire = False)
 
 	def occupancy_grid(self, draw = True):
 
@@ -63,9 +128,9 @@ class scene():
 				self.draw_cell(j)
 				has_pts[j] = 1
 
-		self.occupied = np.where(has_pts == 1)
+		self.occupied = np.asarray(np.where(has_pts == 1)).T
 
-	def draw_cell(self, cell):
+	def draw_cell(self, cell, wire = True):
 		"""draw specified cell number"""
 	
 		#get defining boundary of cell
@@ -81,7 +146,7 @@ class scene():
 		s = [self.grid[cell,0] - self.cw/2, self.grid[cell,0] + self.cw/2, 
 			 self.grid[cell,1] - self.cw/2, self.grid[cell,1] + self.cw/2,
 			 self.grid[cell,2] - self.cw/2, self.grid[cell,2] + self.cw/2 ]
-		b = shapes.Box(size = s, c = [0.2,0.2,0.5]).wireframe(self.wire)
+		b = shapes.Box(size = s, c = [0.2,0.2,0.5]).wireframe(wire)
 		self.disp.append(b)
 
 
