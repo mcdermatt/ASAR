c418898f06e454d3695376a495b2ac087e66452f
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2022-05-19 12:11:58 -0400

    found another bug with how we were drawing ellipses

--- v3/scan_match.py
+++ v3/scan_match.py
@@ -10,50 +10,50 @@ from ICET_spherical import ICET
 from utils import R_tf
 from metpy.calc import lat_lon_grid_deltas
 
-# # KITTI sample dataset -----------------------------------------------------------------
-# basedir = 'C:/kitti/'
-# date = '2011_09_26'
+# KITTI sample dataset -----------------------------------------------------------------
+basedir = 'C:/kitti/'
+date = '2011_09_26'
 
-# # urban dataset used in 3D-ICET paper 
-# drive = '0005'
-# idx = 0
+# urban dataset used in 3D-ICET paper 
+drive = '0005'
+idx = 0
 
-# #test with aiodrive
-# # drive = 'aiodrive'
-# # idx = 1
+#test with aiodrive
+# drive = 'aiodrive'
+# idx = 1
 
-# #alternate dataset with fewer moving objects?
-# # drive = '0009'
-# # idx = 245
-# # drive = '0093'
-# # idx = 220
+#alternate dataset with fewer moving objects?
+# drive = '0009'
+# idx = 245
+# drive = '0093'
+# idx = 220
 
+dataset = pykitti.raw(basedir, date, drive)
+
+# basedir = "E:/KITTI/dataset/"
+# date = "2011_09_26"
+# drive = '01'
 # dataset = pykitti.raw(basedir, date, drive)
 
-# # basedir = "E:/KITTI/dataset/"
-# # date = "2011_09_26"
-# # drive = '01'
-# # dataset = pykitti.raw(basedir, date, drive)
+# idx = 0
 
-# # idx = 0
+velo1 = dataset.get_velo(idx) # Each scan is a Nx4 array of [x,y,z,reflectance]
+c1 = velo1[:,:3]
+velo2 = dataset.get_velo(idx+1) # Each scan is a Nx4 array of [x,y,z,reflectance]
+c2 = velo2[:,:3]
+c1 = c1[c1[:,2] > -1.5] #ignore ground plane
+c2 = c2[c2[:,2] > -1.5] #ignore ground plane
+# c1 = c1[c1[:,2] > -2.] #ignore reflections
+# c2 = c2[c2[:,2] > -2.] #ignore reflections
 
-# velo1 = dataset.get_velo(idx) # Each scan is a Nx4 array of [x,y,z,reflectance]
-# c1 = velo1[:,:3]
-# velo2 = dataset.get_velo(idx+1) # Each scan is a Nx4 array of [x,y,z,reflectance]
-# c2 = velo2[:,:3]
-# # c1 = c1[c1[:,2] > -1.5] #ignore ground plane
-# # c2 = c2[c2[:,2] > -1.5] #ignore ground plane
-# # c1 = c1[c1[:,2] > -2.] #ignore reflections
-# # c2 = c2[c2[:,2] > -2.] #ignore reflections
+#load previously processed cloud 1
+# c1 = np.loadtxt("cloud1_good.txt")
 
-# #load previously processed cloud 1
-# # c1 = np.loadtxt("cloud1_good.txt")
-
-# poses0 = dataset.oxts[idx] #<- ID of 1st scan
-# poses1 = dataset.oxts[idx+1] #<- ID of 2nd scan
-# dt = 0.1037 #mean time between lidar samples
-# OXTS_ground_truth = tf.constant([poses1.packet.vf*dt, -poses1.packet.vl*dt, poses1.packet.vu*dt, poses1.packet.wf*dt, poses1.packet.wl*dt, poses1.packet.wu*dt])
-# # ------------------------------------------------------------------------------------
+poses0 = dataset.oxts[idx] #<- ID of 1st scan
+poses1 = dataset.oxts[idx+1] #<- ID of 2nd scan
+dt = 0.1037 #mean time between lidar samples
+OXTS_ground_truth = tf.constant([poses1.packet.vf*dt, -poses1.packet.vl*dt, poses1.packet.vu*dt, poses1.packet.wf*dt, poses1.packet.wl*dt, poses1.packet.wu*dt])
+# ------------------------------------------------------------------------------------
 
 # # full KITTI dataset (uses different formatting incompable with PyKitti)--------------
 # #files are 80gb so remember to plug in the external hard drive!
@@ -141,28 +141,28 @@ from metpy.calc import lat_lon_grid_deltas
 # #---------------------------------------------------------------------------------------
 
 
-# load custom point cloud geneated in matlab------------------------------------------
-# c1 = np.loadtxt("scene2_scan1.txt", dtype = float) #small cylinders
-# c2 = np.loadtxt("scene2_scan2.txt", dtype = float)
-# c1 = np.loadtxt("scene3_scan1.txt", dtype = float) #rectangles
-# c2 = np.loadtxt("scene3_scan2.txt", dtype = float)
-# c1 = np.loadtxt("scene4_scan1.txt", dtype = float) #cylinders
-# c2 = np.loadtxt("scene4_scan2.txt", dtype = float)
-c1 = np.loadtxt("simple_room_scan1.txt", dtype = float) #for debugging DNN filter
-c2 = np.loadtxt("simple_room_scan2.txt", dtype = float)
+# # load custom point cloud geneated in matlab------------------------------------------
+# # c1 = np.loadtxt("scene2_scan1.txt", dtype = float) #small cylinders
+# # c2 = np.loadtxt("scene2_scan2.txt", dtype = float)
+# # c1 = np.loadtxt("scene3_scan1.txt", dtype = float) #rectangles
+# # c2 = np.loadtxt("scene3_scan2.txt", dtype = float)
+# # c1 = np.loadtxt("scene4_scan1.txt", dtype = float) #cylinders
+# # c2 = np.loadtxt("scene4_scan2.txt", dtype = float)
+# c1 = np.loadtxt("simple_room_scan1.txt", dtype = float) #for debugging DNN filter
+# c2 = np.loadtxt("simple_room_scan2.txt", dtype = float)
 
-# c1 = c1[c1[:,2] > -1.55] #ignore ground plane
-# c2 = c2[c2[:,2] > -1.55] #ignore ground plane
+# # c1 = c1[c1[:,2] > -1.55] #ignore ground plane
+# # c2 = c2[c2[:,2] > -1.55] #ignore ground plane
 
-# x = tf.constant([0., 0.3, 0., 0., 0.0, -0.1])
-# rot = R_tf(x[3:])
-# c2 = c1 @ rot.numpy() + x[:3].numpy()
-# # c2 = (c1 +  x[:3].numpy()) @ rot.numpy()
+# # x = tf.constant([0., 0.3, 0., 0., 0.0, -0.1])
+# # rot = R_tf(x[3:])
+# # c2 = c1 @ rot.numpy() + x[:3].numpy()
+# # # c2 = (c1 +  x[:3].numpy()) @ rot.numpy()
 
-#add noise (if not generated when point clouds were created)
-c1 += 0.02*np.random.randn(np.shape(c1)[0], 3)
-c2 += 0.02*np.random.randn(np.shape(c2)[0], 3) 
-# ------------------------------------------------------------------------------------
+# #add noise (if not generated when point clouds were created)
+# c1 += 0.02*np.random.randn(np.shape(c1)[0], 3)
+# c2 += 0.02*np.random.randn(np.shape(c2)[0], 3) 
+# # ------------------------------------------------------------------------------------
 
 # # #single distinct cluster---------------------------------------------------------------
 # c1 = np.random.randn(3000,3)*tf.constant([0.3,0.04,0.3]) + tf.constant([0.,8.,0.])
@@ -170,7 +170,7 @@ c2 += 0.02*np.random.randn(np.shape(c2)[0], 3)
 # # # c2 = c1 - np.array([0.1, 0.3, 0.0])
 # # # -------------------------------------------------------------------------------------
 
-it1 = ICET(cloud1 = c1, cloud2 = c2, fid = 30, niter = 13, 
+it1 = ICET(cloud1 = c1, cloud2 = c2, fid = 50, niter = 13, 
 	draw = True, group = 2, RM = False, DNN_filter = False) #, cheat = ground_truth)
 
 print("\n OXTS_ground_truth: \n", OXTS_ground_truth)
