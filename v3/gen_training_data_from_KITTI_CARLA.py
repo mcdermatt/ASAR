ba26be106cafefe51177ae58202fb4188407e2cb
mcdermatt <mcdermatt@gmail.com>
2022-10-18 18:33:34 -0400

    writing scripts to import KITTICARLA data, hopefully will improve training with better ground truth

---
+++ v3/gen_training_data_from_KITTI_CARLA.py
@@ -0,0 +1,147 @@
+"""generates data for training perspective shift filter DNN from KITTI sample dataset"""
+
+import pykitti
+import numpy as np
+import tensorflow as tf
+
+#limit GPU memory ------------------------------------------------
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+  try:
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+  except RuntimeError as e:
+    print(e)
+#-----------------------------------------------------------------
+
+from tensorflow.math import sin, cos, tan
+import tensorflow_probability as tfp
+from ICET_spherical import ICET
+from utils import R_tf
+from metpy.calc import lat_lon_grid_deltas
+
+numShifts = 10 #5 #number of times to resample and translate each voxel each scan
+runLen = 200
+start_idx = 800
+npts = 100 #50 
+skip3 = True #only consider each third point cloud (increase perspective shift bias)
+
+
+for idx in range(runLen):
+	
+	s1_fn = '/home/derm/KITTICARLA/dataset/Town02/generated/frames/frame_%04d.ply' %(idx+start_idx)
+
+
+
+	velo1 = dataset.get_velo(idx) # Each scan is a Nx4 array of [x,y,z,reflectance]
+	c1 = velo1[:,:3]
+	if skip3 == True:
+		velo2 = dataset.get_velo(idx+3) # Each scan is a Nx4 array of [x,y,z,reflectance]
+	else:
+		velo2 = dataset.get_velo(idx+1)
+	c2 = velo2[:,:3]
+	c1 = c1[c1[:,2] > -1.5] #ignore ground plane
+	c2 = c2[c2[:,2] > -1.5] #ignore ground plane
+	# c1 = c1[c1[:,2] > -2.] #ignore reflections
+	# c2 = c2[c2[:,2] > -2.] #ignore reflections
+
+	dt = 0.1037 #mean time between lidar samples
+	poses0 = dataset.oxts[idx] #<- ID of 1st scan
+	poses1 = dataset.oxts[idx+1] #<- ID of 2nd scan
+	if skip3 == False:
+		OXTS_ground_truth = tf.constant([poses1.packet.vf*dt, -poses1.packet.vl*dt, poses1.packet.vu*dt, poses1.packet.wf*dt, poses1.packet.wl*dt, poses1.packet.wu*dt])
+	else:
+		poses2 = dataset.oxts[idx+2]
+		poses3 = dataset.oxts[idx+3]
+		gt1 = tf.constant([poses1.packet.vf*dt, -poses1.packet.vl*dt, poses1.packet.vu*dt, poses1.packet.wf*dt, poses1.packet.wl*dt, poses1.packet.wu*dt])
+		gt2 = tf.constant([poses2.packet.vf*dt, -poses2.packet.vl*dt, poses2.packet.vu*dt, poses2.packet.wf*dt, poses2.packet.wl*dt, poses2.packet.wu*dt])
+		gt3 = tf.constant([poses3.packet.vf*dt, -poses3.packet.vl*dt, poses3.packet.vu*dt, poses3.packet.wf*dt, poses3.packet.wl*dt, poses3.packet.wu*dt])
+		OXTS_ground_truth = gt1 + gt2 + gt3
+
+	shift_scale = 0.0 #standard deviation by which to shift the grid BEFORE SAMPLING corresponding segments of the point cloud
+	shift = tf.cast(tf.constant([shift_scale*tf.random.normal([1]).numpy()[0], shift_scale*tf.random.normal([1]).numpy()[0], 0.2*shift_scale*tf.random.normal([1]).numpy()[0], 0, 0, 0]), tf.float32)
+
+	it = ICET(cloud1 = c1, cloud2 = c2, fid = 70, niter = 2, draw = False, group = 2, 
+		RM = True, DNN_filter = False, cheat = OXTS_ground_truth+shift)
+
+	#Get ragged tensor containing all points from each scan inside each sufficient voxel
+	in1 = it.inside1
+	npts1 = it.npts1
+	in2 = it.inside2
+	npts2 = it.npts2
+	corr = it.corr #indices of bins that have enough points from scan1 and scan2
+
+	#get indices of rag with >= 25 elements
+	ncells = tf.shape(corr)[0].numpy() #num of voxels with sufficent number of points
+	# print(tf.gather(npts2, corr))
+	enough1 = tf.gather(in1, corr)
+	enough2 = tf.gather(in2, corr)
+
+	for j in range(numShifts):
+		#init array to store indices
+		idx1 = np.zeros([ncells ,npts])
+		idx2 = np.zeros([ncells ,npts])
+
+		#loop through each element of ragged tensor
+		for i in range(ncells):
+		    idx1[i,:] = tf.random.shuffle(enough1[i])[:npts].numpy() #shuffle order and take first 25 elements
+		    idx2[i,:] = tf.random.shuffle(enough2[i])[:npts].numpy() #shuffle order and take first 25 elements
+
+		idx1 = tf.cast(tf.convert_to_tensor(idx1), tf.int32) #indices in scan 1 of points we've selected
+		idx2 = tf.cast(tf.convert_to_tensor(idx2), tf.int32) 
+
+		from1 = tf.gather(it.cloud1_tensor, idx1)
+		# from2 = tf.gather(it.cloud2_tensor_OG, idx2) #corresponding points in OG pose
+		from2 = tf.gather(it.cloud2_tensor, idx2) 	   #transformed by "ground truth" translation
+
+		scan1 = tf.reshape(from1, [-1, 3]).numpy()
+		scan2 = tf.reshape(from2, [-1, 3]).numpy()
+
+		#randomly translate each sample from scan 2
+		rand = tf.constant([1., 1., 0.1])*tf.random.normal([ncells, 3])
+		#tile and apply to scan2
+		t = tf.tile(rand, [npts,1])
+		t = tf.reshape(tf.transpose(t), [3,npts,-1])
+		t = tf.transpose(t, [2,1,0])
+		t = tf.reshape(t, [-1, 3])
+		scan2 += t.numpy()
+
+		full_soln_vec = rand + shift[:3]
+		compact_soln_vec = it.L @ tf.transpose(it.U, [0,2,1]) @ full_soln_vec[:,:,None] #remove extended axis
+		compact_soln_vec = tf.matmul(it.U, compact_soln_vec) #project back to XYZ
+		compact_soln_vec = compact_soln_vec[:,:,0] #get rid of extra dimension
+
+		soln = full_soln_vec #consider entire solution vector (compact and extended directions)
+		# soln = compact_soln_vec #only consider ground truth solutions in directions deemed useful by ICET
+
+		#initialize array to store data on first iteration
+		if idx*(j+1) == 0:
+			scan1_cum = scan1
+			scan2_cum = scan2
+			soln_cum = rand + shift[:3]
+		else:
+			scan1_cum = np.append(scan1_cum, scan1, axis = 0)
+			scan2_cum = np.append(scan2_cum, scan2, axis = 0)
+			soln_cum = np.append(soln_cum, soln, axis = 0)
+
+		#test
+		# print("\n full_soln_vec:", tf.shape(full_soln_vec))
+		# print("\n compact_soln_vec:", tf.shape(compact_soln_vec))
+		# print("t", tf.shape(t))
+		# print("U_i", tf.shape(it.U))
+
+
+	print("got", tf.shape(enough2.to_tensor())[0].numpy()*numShifts, "training samples from scan", idx)
+
+#smol
+# np.savetxt('perspective_shift/training_data/ICET_KITTI_scan1_25_shifted.txt', scan1_cum)
+# np.savetxt('perspective_shift/training_data/ICET_KITTI_scan2_25_shifted.txt', scan2_cum)
+# np.savetxt('perspective_shift/training_data/ICET_KITTI_ground_truth_25_shifted.txt', soln_cum)
+
+#big
+np.save('/media/derm/06EF-127D1/TrainingData/KITTI_0091_scan1_100pts_skip3', scan1_cum)
+np.save('/media/derm/06EF-127D1/TrainingData/KITTI_0091_scan2_100pts_skip3', scan2_cum)
+np.save('/media/derm/06EF-127D1/TrainingData/KITTI_0091_ground_truth_100pts_skip3', soln_cum)
+
+#0071_100pts - 500 frames ---> rm > 0.2
+#		Note: 71 has lots of pedestrians
