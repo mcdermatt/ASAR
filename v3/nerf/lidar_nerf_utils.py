found and fixed bug with phiminmax patches, accumulating gradients between images helps smooth out imperfections in gt poses

v3/nerf/lidar_nerf_utils.py
@@ -40,7 +40,7 @@ def posenc(x, embed_dims):
       rets.append(fn(2.**i * x))
   return tf.concat(rets, -1)
 
-#2**18 is waaaaay too fine to learn anything on most of the channels!
+#2**18 is waaaaay too fine to learn anything on most of the channels!?!
 # L_embed =  5 #18 #15 #10 #6
 embed_fn = posenc
 
@@ -50,25 +50,24 @@ def init_model(D=8, W=256): #8,256
     dense = lambda W=W, act=relu : tf.keras.layers.Dense(W, activation=act, kernel_initializer='glorot_uniform')
 
 #     inputs = tf.keras.Input(shape=(3 + 3*2*L_embed)) #old (embed everything together)
-    inputs = tf.keras.Input(shape=(6 + 3*2*(4) + 3*2*(10))) #new (embedding dims (4) and (10) )
+    inputs = tf.keras.Input(shape=(6 + 3*2*(4) + 3*2*(14))) #new (embedding dims (4) and (10) )
     # outputs = inputs #old
-    outputs = inputs[:,:(3+3*2*(10))] #only look at positional stuff for now
+    outputs = inputs[:,:(3+3*2*(14))] #only look at positional stuff for now
 
     #try removing view dependant effects from density 
 
     for i in range(D):
         outputs = dense()(outputs)
-        #test-- add layer normalization
-        outputs = tf.keras.layers.LayerNormalization()(outputs)
+        outputs = tf.keras.layers.LayerNormalization()(outputs) #as recomended by LOC-NDF 
         
         if i%4==0 and i>0:
-            outputs = tf.concat([outputs, inputs[:,:(3+3*2*(10))]], -1)
+            outputs = tf.concat([outputs, inputs[:,:(3+3*2*(14))]], -1)
 
     #extend small MLP after output of density channel to get ray drop
     sigma_channel = dense(1, act=None)(outputs)    
-    rd_start = tf.concat([outputs, inputs[:,(3+3*2*(10)):]], -1)
-#     rd_channel = dense(128, act=relu)(outputs) #was this
-    rd_channel = dense(256, act=relu)(outputs) #test matching OG NeRF structure
+    rd_start = tf.concat([outputs, inputs[:,(3+3*2*(14)):]], -1)
+    rd_channel = dense(256, act=relu)(outputs) #OG NeRF structure
+    # rd_channel = dense(256, act=relu)(outputs) #test adding another channel
     rd_channel = dense(128, act=relu)(rd_channel)
     rd_channel = dense(1, act=tf.keras.activations.sigmoid)(rd_channel)
     out = tf.concat([sigma_channel, rd_channel], -1)
@@ -76,20 +75,38 @@ def init_model(D=8, W=256): #8,256
     
     return model
 
+def cylindrical_to_cartesian(pts):
+
+    x = pts[:,0]*tf.math.cos(pts[:,1])
+    y = pts[:,0]*tf.math.sin(pts[:,1]) 
+    z = pts[:,2]
+
+    out = tf.transpose(tf.Variable([x, y, z]))
+
+    return(out)
+
+
 def get_rays(H, W, c2w, phimin_patch, phimax_patch):
     i, j = tf.meshgrid(tf.range(W, dtype=tf.float32), tf.range(H, dtype=tf.float32), indexing='xy')
 
-    phimin = np.deg2rad(-16) 
-    phimax = np.deg2rad(17.75)
-
+    # phimin = np.deg2rad(-16) 
+    # phimax = np.deg2rad(17.75)
+    # phimin = np.deg2rad(-17.75)  #test
+    # phimax = np.deg2rad(16.) #test
+    phimin = np.deg2rad(-15.594) #should be this?
+    phimax = np.deg2rad(17.743)
+    # phimin = np.deg2rad(-17.743) #try flipping observed range only here(?)
+    # phimax = np.deg2rad(15.594) 
+    
     #~~~~~~~~~~~~~~~~~~~~~~~~~
 #     #Cylindrical projection model (new)
-#     #only seems to draw well with single vertical patch
 #     dirs_test = tf.stack([-tf.ones_like(i), #r
 #                       #theta
-#                       (i - (1024//(2*n_rots)))  /(2048//(2*n_rots)) * (2*np.pi/n_rots) + np.pi, #for uninterpolated images
+#                       # (i - (1024//(2*n_rots)))  /(2048//(2*n_rots)) * (2*np.pi/n_rots) + np.pi, #for uninterpolated images
+#                       (i - (W//2))  /(W) * (2*np.pi/(1024//W)) + np.pi, #just use W
 #                       #phi
-#                       (phimax_patch + phimin_patch)/2 - ((-j+(32//n_vert_patches))/(64//n_vert_patches))*(phimax_patch-phimin_patch) 
+#                       # (phimax_patch + phimin_patch)/2 - ((-j+(32//n_vert_patches))/(64//n_vert_patches))*(phimax_patch-phimin_patch)
+#                       (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) #- np.pi/2
 #                      ], -1)
 #     dirs_test = tf.reshape(dirs_test,[-1,3])
 # #     dirs_test = LC.s2c(LC, dirs_test)    #was this
@@ -107,7 +124,9 @@ def get_rays(H, W, c2w, phimin_patch, phimax_patch):
                           #phi
                           #need to manually account for elevation angle of patch 
                           #  (can not be inferred from c2w since that does not account for singularities near "poles" of spherical projection)
-                          (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) -np.pi/2
+                          # (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) -np.pi/2 #slightly wrong
+                        # (phimax_patch + phimin_patch)/2 + ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #better!
+                        (phimax_patch + phimin_patch)/2 - ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #test
                          ], -1)
     dirs_test = tf.reshape(dirs_test,[-1,3])
     dirs_test = LC.s2c(LC, dirs_test)     
@@ -147,7 +166,7 @@ def render_rays(network_fn, rays_o, rays_d, z_vals):
 
     ray_pos = rays_o[...,None,:] + rays_d[...,None,:] * z_vals
     ray_pos_flat = tf.reshape(ray_pos, [-1, 3])
-    encoded_ray_pos = embed_fn(ray_pos_flat, 10) #10 embedding dims for pos
+    encoded_ray_pos = embed_fn(ray_pos_flat, 14) #10 embedding dims for pos
     ray_dir = tf.reshape(rays_d[..., None,:]*tf.ones_like(z_vals, dtype = tf.float32), [-1,3]) #test
     encoded_ray_dir = embed_fn(ray_dir, 4)  # embedding dims for dir
 
@@ -160,7 +179,7 @@ def render_rays(network_fn, rays_o, rays_d, z_vals):
 #     print("encoded_both", np.shape(encoded_both))
     raw = batchify(network_fn)(encoded_both) #old
 
-#     print("raw", np.shape(raw))
+    # print("raw", np.shape(raw))
     
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # print("problem here: ", [ray_pos.shape[0],ray_pos.shape[1],-1,2])    
@@ -199,24 +218,28 @@ def calculate_loss(depth, ray_drop, target, target_drop_mask):
     target_nondrop = tf.math.multiply(target, target_drop_mask)
     L_dist = tf.reduce_mean(tf.abs(depth_nondrop - target_nondrop))
     
-    #Gradient Loss (structural regularization for smooth surfaces)
+    #Gradient Loss (structural regularization for smooth surfaces) -- (LiDAR-NeRF method) ~~~~~~~~~~~
 #     thresh = 0.025 #was at 0.025, set to 0.1 in LiDAR-NeRF
     ##--seems like this works better if I set different values for each component
     #    this makes sense since the resolution of the sensor is differnt in horizontal and vertical(?)
-    thresh_horiz = 0.001 
-    thresh_vert = 0.01
-    mask = np.zeros(np.shape(target[:,:,0]))
+    thresh_horiz = 0.025 
+    thresh_vert = 0.025
+    # thresh_horiz = 0.001 #test 
+    # thresh_vert = 0.01 #test
+    mask = np.ones(np.shape(target[:,:,0]))
     vertical_grad_target = np.gradient(target[:,:,0])[0] 
-    #test for double gradient 
-    vertical_grad_target2 = np.gradient(vertical_grad_target)[0] 
-    vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target2) > thresh_vert)
+    vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target) > thresh_vert) #old
+    # #test for double gradient 
+    # vertical_grad_target2 = np.gradient(vertical_grad_target)[0] 
+    # vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target2) > thresh_vert)
 
-    mask[vertical_past_thresh[:,0], vertical_past_thresh[:,1]] = 1
+    mask[vertical_past_thresh[:,0], vertical_past_thresh[:,1]] = 0 #1
     horizontal_grad_target = np.gradient(target[:,:,0])[1]
-    #test for double gradient
-    horizontal_grad_target2 = np.gradient(horizontal_grad_target)[1]  
-    horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target2) > thresh_horiz)
-    mask[horizontal_past_thresh[:,0], horizontal_past_thresh[:,1]] = 1
+    horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target) > thresh_horiz) #old
+    # #test for double gradient
+    # horizontal_grad_target2 = np.gradient(horizontal_grad_target)[1]  
+    # horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target2) > thresh_horiz)
+    mask[horizontal_past_thresh[:,0], horizontal_past_thresh[:,1]] = 0 #1
     
     vertical_grad_inference = np.gradient(depth[:,:,0])[0]
     horizontal_grad_inference = np.gradient(depth[:,:,0])[1]
@@ -228,9 +251,11 @@ def calculate_loss(depth, ray_drop, target, target_drop_mask):
     L_reg = np.multiply(mag_difference, mask)
     L_reg = L_reg[:,:,None]
     L_reg = tf.reduce_mean(tf.math.multiply(L_reg, target_drop_mask))
-    L_reg = tf.cast(L_reg, tf.float32)            
+    L_reg = tf.cast(L_reg, tf.float32)    
 
-    lam1 = 1 #100
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        
+
+    lam1 = 0 #100 #100 #10 #100
     lam2 = 1 #1/(64**2)
     loss = L_dist + lam1*L_reg + lam2*L_raydrop       
 