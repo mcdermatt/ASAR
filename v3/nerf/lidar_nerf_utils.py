061015abd8553babd77f5d51b535df3bfb01ce19
mcdermatt <mcdermatt@gmail.com>
2024-05-13 22:50:40 -0400

    more debugging NeRF training data, starting work on synthetic scans from HD map

--- v3/nerf/lidar_nerf_utils.py
+++ v3/nerf/lidar_nerf_utils.py
@@ -89,15 +89,14 @@ def cylindrical_to_cartesian(pts):
 def get_rays(H, W, c2w, phimin_patch, phimax_patch):
     i, j = tf.meshgrid(tf.range(W, dtype=tf.float32), tf.range(H, dtype=tf.float32), indexing='xy')
 
-    # phimin = np.deg2rad(-16) 
+    # phimin = np.deg2rad(-16)  #old
     # phimax = np.deg2rad(17.75)
-    # phimin = np.deg2rad(-17.75)  #test
-    # phimax = np.deg2rad(16.) #test
-    phimin = np.deg2rad(-15.594) #should be this?
-    phimax = np.deg2rad(17.743)
-    # phimin = np.deg2rad(-17.743) #try flipping observed range only here(?)
-    # phimax = np.deg2rad(15.594) 
-    
+    # phimin = np.deg2rad(-15.594) #used this for v10
+    # phimax = np.deg2rad(17.743)
+    # phimin = np.deg2rad(-5)  #TEST
+    # phimax = np.deg2rad(27)
+    # phimax = np.deg2rad(15.594) #test
+    # phimin = np.deg2rad(-17.743)    
     #~~~~~~~~~~~~~~~~~~~~~~~~~
 #     #Cylindrical projection model (new)
 #     dirs_test = tf.stack([-tf.ones_like(i), #r
@@ -106,7 +105,8 @@ def get_rays(H, W, c2w, phimin_patch, phimax_patch):
 #                       (i - (W//2))  /(W) * (2*np.pi/(1024//W)) + np.pi, #just use W
 #                       #phi
 #                       # (phimax_patch + phimin_patch)/2 - ((-j+(32//n_vert_patches))/(64//n_vert_patches))*(phimax_patch-phimin_patch)
-#                       (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) #- np.pi/2
+#                       # (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) #- np.pi/2
+#                       (phimax_patch + phimin_patch)/2 - ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) #-np.pi/2 #using 5/1
 #                      ], -1)
 #     dirs_test = tf.reshape(dirs_test,[-1,3])
 # #     dirs_test = LC.s2c(LC, dirs_test)    #was this
@@ -126,14 +126,16 @@ def get_rays(H, W, c2w, phimin_patch, phimax_patch):
                           #  (can not be inferred from c2w since that does not account for singularities near "poles" of spherical projection)
                           # (phimax_patch + phimin_patch)/2 + ((-j+(H/2))/(H))*(phimax_patch-phimin_patch) -np.pi/2 #slightly wrong
                         # (phimax_patch + phimin_patch)/2 + ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #better!
-                        (phimax_patch + phimin_patch)/2 - ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #test
+                        (phimax_patch + phimin_patch)/2 - ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #using 5/1
+                        # (phimax_patch + phimin_patch)/2 + ((-j+((H-1)/2))/(H-1))*(phimax_patch-phimin_patch) -np.pi/2 #TEST--flip order within patch
                          ], -1)
     dirs_test = tf.reshape(dirs_test,[-1,3])
     dirs_test = LC.s2c(LC, dirs_test)     
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~
     
-    rotm = R.from_euler('xyz', [0,-np.pi/2 + (phimax + phimin)/2,0]).as_matrix()
+    # rotm = R.from_euler('xyz', [0,-np.pi/2 + (phimax + phimin)/2,0]).as_matrix() #was this --> think I'm double counting horizon centering
+    rotm = R.from_euler('xyz', [0,-np.pi/2,0]).as_matrix() #was this
     dirs_test = dirs_test @ rotm
     dirs_test = dirs_test @ tf.transpose(c2w[:3,:3])
     dirs = dirs_test @ (c2w[:3,:3] 
@@ -228,24 +230,24 @@ def calculate_loss(depth, ray_drop, target, target_drop_mask):
     # thresh_vert = 0.01 #test
     mask = np.ones(np.shape(target[:,:,0]))
     vertical_grad_target = np.gradient(target[:,:,0])[0] 
-    vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target) > thresh_vert) #old
+    # vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target) > thresh_vert) #old
     # #test for double gradient 
-    # vertical_grad_target2 = np.gradient(vertical_grad_target)[0] 
-    # vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target2) > thresh_vert)
+    vertical_grad_target2 = np.gradient(vertical_grad_target)[0] 
+    vertical_past_thresh = np.argwhere(tf.abs(vertical_grad_target2) > thresh_vert)
 
     mask[vertical_past_thresh[:,0], vertical_past_thresh[:,1]] = 0 #1
     horizontal_grad_target = np.gradient(target[:,:,0])[1]
-    horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target) > thresh_horiz) #old
+    # horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target) > thresh_horiz) #old
     # #test for double gradient
-    # horizontal_grad_target2 = np.gradient(horizontal_grad_target)[1]  
-    # horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target2) > thresh_horiz)
+    horizontal_grad_target2 = np.gradient(horizontal_grad_target)[1]  
+    horizontal_past_thresh = np.argwhere(tf.abs(horizontal_grad_target2) > thresh_horiz)
     mask[horizontal_past_thresh[:,0], horizontal_past_thresh[:,1]] = 0 #1
     
     vertical_grad_inference = np.gradient(depth[:,:,0])[0]
     horizontal_grad_inference = np.gradient(depth[:,:,0])[1]
-    mag_difference = tf.math.sqrt((vertical_grad_target-vertical_grad_inference)**2 + (horizontal_grad_target-horizontal_grad_inference)**2)
+    # mag_difference = tf.math.sqrt((vertical_grad_target-vertical_grad_inference)**2 + (horizontal_grad_target-horizontal_grad_inference)**2)
     #DEBUG -- use struct reg. to amplify LR in sharp corners 
-#     mag_difference = tf.reduce_mean(tf.abs(depth_nondrop - target_nondrop)) 
+    mag_difference = tf.reduce_mean(tf.abs(depth_nondrop - target_nondrop)) 
 
     #suppress ray drop areas (for distance and gradient loss)
     L_reg = np.multiply(mag_difference, mask)
@@ -255,8 +257,174 @@ def calculate_loss(depth, ray_drop, target, target_drop_mask):
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        
 
-    lam1 = 0 #100 #100 #10 #100
+    lam1 = 100 #100 #100 #10 #100
     lam2 = 1 #1/(64**2)
     loss = L_dist + lam1*L_reg + lam2*L_raydrop       
 
     return(loss)
+
+
+def apply_motion_profile(cloud_xyz, m_hat, period_lidar = 1):
+    """Linear correction for motion distortion, using ugly python code
+
+    cloud_xyz: distorted cloud in Cartesian space
+    m_hat: estimated motion profile for linear correction
+    period_lidar: time it takes for LIDAR sensor to record a single sweep
+    """
+
+    period_base = (2*np.pi)/m_hat[-1]
+
+    #remove inf values
+    cloud_xyz = cloud_xyz[cloud_xyz[:,0] < 10_000]
+    #convert to spherical coordinates
+    # cloud_spherical = c2s(cloud_xyz).numpy()
+
+    #Because of body frame yaw rotation, we're not always doing a full roation - we need to "uncurl" initial point cloud
+    # (this is NOT baked in to motion profile)
+    # cloud_spherical = cloud_spherical[np.argsort(cloud_spherical[:,1])] #sort by azim angle
+    #get total overlap in rotation between LIDAR and base frames (since both are rotating w.r.t. world Z)
+    # point of intersection = (t_intersection) * (angular velocity base)
+    #                       = ((n * T_a * T_b) / (T_a + T_b)) * omega_base 
+    # total_rot = -2*np.pi*np.sin(m_hat[-1]/(-m_hat[-1] + (2*np.pi/period_lidar)))
+    # print("total_rot:", total_rot)
+
+    #scale linearly starting at theta = 0
+    # cloud_spherical[:,1] = ((cloud_spherical[:,1]) % (2*np.pi))*((2*np.pi - total_rot)/(2*np.pi)) + total_rot #works
+
+    #sort by azim angle again- some points will have moved past origin in the "uncurling" process
+    # cloud_spherical = cloud_spherical[np.argsort(cloud_spherical[:,1])] 
+
+    #reorient
+    # cloud_spherical[:,1] = ((cloud_spherical[:,1] + np.pi) % (2*np.pi)) - np.pi
+    # cloud_xyz = s2c(cloud_spherical).numpy() #convert back to xyz
+
+    rectified_vel  = -m_hat[None,:]
+    # rectified_vel[0,-1] = 0 #zero out yaw since we already compensated for it
+
+    T = (2*np.pi)/(-m_hat[-1] + (2*np.pi/period_lidar)) #time to complete 1 scan #was this
+    # print(T)
+    rectified_vel = rectified_vel * T #was this
+    # print(rectified_vel[:,-1])
+    # rectified_vel[:-1] = rectified_vel[:-1] * T #nope
+    # rectified_vel[:,-1] = rectified_vel[:,-1] * T #also nope
+
+    # linearly spaced motion profile ~~~~~~~~~~~~~~~~~~~~~
+    # this is a bad way of doing it ... what happens if most of the points are on one half of the scene??
+
+    # part2 = np.linspace(0.5, 1.0, len(cloud_xyz)//2)[:,None]
+    # part1 = np.linspace(0, 0.5, len(cloud_xyz) - len(cloud_xyz)//2)[:,None]
+    # motion_profile = np.append(part1, part2, axis = 0) @ rectified_vel
+
+    # # using yaw angles ~~~~~~~~~~~~~~~~~~~~~~
+    #  (NEW)
+    
+    #TODO: need to center point cloud before getting yaw angles
+
+    yaw_angs = cartesian_to_spherical(cloud_xyz)[:,1].numpy()
+    last_subzero_idx = int(len(yaw_angs) // 8)
+    yaw_angs[last_subzero_idx:][yaw_angs[last_subzero_idx:] < 0.3] = yaw_angs[last_subzero_idx:][yaw_angs[last_subzero_idx:] < 0.3] + 2*np.pi
+    # yaw_angs = yaw_angs / (2*np.pi) #test
+    # yaw_angs = yaw_angs[:,None]  #test
+
+    # #test
+    # self.yaw_angs = yaw_angs
+    # self.cloud_xyz = cloud_xyz
+    # color = 255*self.yaw_angs/(2*np.pi)
+    # cname = np.array([255-color, color, 255-color]).T.tolist()
+    # # self.disp.append(Points(self.cloud_xyz + 0.01*np.random.randn(np.shape(cloud_xyz)[0],3), c=cname, r = 3))
+    # self.disp.append(Points(self.cloud_xyz, c=cname, r = 3))
+
+    #jump in <yaw_angs> is causing unintended behavior in real world LIDAR data
+    yaw_angs = (yaw_angs + 2*np.pi)%(2*np.pi)
+
+    #TODO: should I use (2pi - T) in place of max(yaw_angs) -> ???
+    motion_profile = (yaw_angs / np.max(yaw_angs))[:,None] @ rectified_vel #was this
+    # motion_profile = yaw_angs[:,None] @ rectified_vel #test
+    # print("\n new: \n", motion_profile[:,0])
+    # self.yaw_angs_new = yaw_angs
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    #Apply motion profile
+    # # Old loopy method ~~~~~~~~~~~~~~
+    # T = []
+    # for i in range(len(motion_profile)):
+    #   tx, ty, tz, roll, pitch, yaw = motion_profile[i]
+    #   R = np.dot(np.dot(np.array([[1, 0, 0], 
+    #                               [0, np.cos(roll), -np.sin(roll)], 
+    #                               [0, np.sin(roll), np.cos(roll)]]), 
+    #                   np.array([[np.cos(pitch), 0, np.sin(pitch)], 
+    #                             [0, 1, 0], 
+    #                             [-np.sin(pitch), 0, np.cos(pitch)]])), 
+    #                   np.array([[np.cos(yaw), -np.sin(yaw), 0], 
+    #                             [np.sin(yaw), np.cos(yaw), 0], 
+    #                             [0, 0, 1]]))
+    #   T.append(np.concatenate((np.concatenate((R, np.array([[tx], [ty], [tz]])), axis=1), np.array([[0, 0, 0, 1]])), axis=0))
+    
+    # #should be the same size:
+    # # print(len(T))
+    # # Apply inverse of motion transformation to each point
+    # undistorted_pc = np.zeros_like(cloud_xyz)
+    # for i in range(len(cloud_xyz)):
+    #   point = np.concatenate((cloud_xyz[i], np.array([1])))
+    #   T_inv = np.linalg.inv(T[i])
+    #   corrected_point = np.dot(T_inv, point)[:3]
+    #   undistorted_pc[i] = corrected_point
+    # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # new method ~~~~~~~~~~~~~~~~~~~~~~
+
+    x = -motion_profile[:,0]
+    y = -motion_profile[:,1]
+    z = -motion_profile[:,2]
+    phi = motion_profile[:,3]
+    theta = motion_profile[:,4]
+    psi = motion_profile[:,5]
+
+    #need to inverse this
+    # T_rect_numpy = np.array([[cos(psi)*cos(theta), sin(phi)*sin(theta)*cos(psi) - sin(psi)*cos(phi), sin(phi)*sin(psi) + sin(theta)*cos(phi)*cos(psi), -x], [sin(psi)*cos(theta), sin(phi)*sin(psi)*sin(theta) + cos(phi)*cos(psi), -sin(phi)*cos(psi) + sin(psi)*sin(theta)*cos(phi), -y], [-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta), -z], [np.zeros(len(x)), np.zeros(len(x)), np.zeros(len(x)), np.ones(len(x))]])
+    #to this
+    T_rect_numpy = np.array([[cos(psi)*cos(theta), sin(psi)*cos(theta), -sin(theta), x*cos(psi)*cos(theta) + y*sin(psi)*cos(theta) - z*sin(theta)], [sin(phi)*sin(theta)*cos(psi) - sin(psi)*cos(phi), sin(phi)*sin(psi)*sin(theta) + cos(phi)*cos(psi), sin(phi)*cos(theta), x*(sin(phi)*sin(theta)*cos(psi) - sin(psi)*cos(phi)) + y*(sin(phi)*sin(psi)*sin(theta) + cos(phi)*cos(psi)) + z*sin(phi)*cos(theta)], [sin(phi)*sin(psi) + sin(theta)*cos(phi)*cos(psi), -sin(phi)*cos(psi) + sin(psi)*sin(theta)*cos(phi), cos(phi)*cos(theta), x*(sin(phi)*sin(psi) + sin(theta)*cos(phi)*cos(psi)) - y*(sin(phi)*cos(psi) - sin(psi)*sin(theta)*cos(phi)) + z*cos(phi)*cos(theta)], [np.zeros(len(x)), np.zeros(len(x)), np.zeros(len(x)), np.ones(len(x))]])
+    T_rect_numpy = np.transpose(T_rect_numpy, (2,0,1))
+    # print(np.shape(cloud_xyz))
+    cloud_homo = np.append(cloud_xyz, np.ones([len(cloud_xyz),1]), axis = 1)
+    # print("cloud homo", np.shape(cloud_homo))
+
+    # undistorted_pc =  (np.linalg.pinv(T_rect_numpy) @ cloud_homo[:,:,None]).astype(np.float32)
+    undistorted_pc =  (T_rect_numpy @ cloud_homo[:,:,None]).astype(np.float32)
+    # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    return undistorted_pc[:,:3,0]
+
+
+def spherical_to_cartesian(pts):
+    """converts spherical -> cartesian"""
+
+    x = pts[:,0]*tf.math.sin(pts[:,2])*tf.math.cos(pts[:,1])
+    y = pts[:,0]*tf.math.sin(pts[:,2])*tf.math.sin(pts[:,1]) 
+    z = pts[:,0]*tf.math.cos(pts[:,2])
+
+    out = tf.transpose(tf.Variable([x, y, z]))
+    # out = tf.Variable([x, y, z])
+    return(out)
+
+#test
+def cylindrical_to_cartesian(pts):
+
+    x = pts[:,0]*tf.math.cos(pts[:,1])
+    y = pts[:,0]*tf.math.sin(pts[:,1]) 
+    z = pts[:,2]
+
+    out = tf.transpose(tf.Variable([x, y, z]))
+
+    return(out)
+
+def cartesian_to_spherical(pts):
+    """ converts points from cartesian coordinates to spherical coordinates """
+    r = tf.sqrt(pts[:,0]**2 + pts[:,1]**2 + pts[:,2]**2)
+    phi = tf.math.acos(pts[:,2]/r)
+    theta = tf.math.atan2(pts[:,1], pts[:,0])
+
+    out = tf.transpose(tf.Variable([r, theta, phi]))
+    return(out)
